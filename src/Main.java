619efd7d5c67c9e0ab975fa364202ffc17578dbd
Michi <michi@michis-laptop.htl.grieskirchen.local>
2023-01-10 15:45:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("f+rt ");
+        neger();
+    }
+
+    public static void neger(){
+        System.out.println("neger");
+    }
+}
